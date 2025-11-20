7154cc9db943966418721b8cedd80dde8893095b
azzayou <azzayou9@gmail.com>
2025-11-20 16:09:17 +0100

    Create update_apt.sh for APT package management
    Add script to update and upgrade APT packages with cleanup commands.

---
+++ update_apt.sh
@@ -0,0 +1,9 @@
+### THIS WILL WORK ONLY IF YOUR PACKAGE MANAGER IS `APT`
+sudo apt update -y && sudo apt upgrade  && sudo apt full-upgrade 
+sudo apt autopurge  && sudo apt autoremove  && sudo apt autoclean 
+sudo aptitude purge ~c  # This to remove all UNUSED apps or packages and just it :]
+### This if you have flatpak then remove the `#` at the beginning next line :] IF you don't have flatpak just keep it OR remove ALL NEXT LINE(NOT RECOMMENDED)
+#sudo flatpak update  
+### To run this programe use 'bash upgrade.sh' or use `chmod u+x update.sh` to run './upgrade.sh' without use bash every time
+sudo -k  # This to exit the super user mode (root account)
+### NOTE : you can return it run every time you login (AUTOMATIC) but this you need to figure out by yourself SOME SEARSH WON't HURT YOU  ^_^ 
